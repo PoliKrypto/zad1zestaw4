ce217217a254f251917955888f263fa8f3f20a0f
217711 Szmigiel Kacper <217711@edu.p.lodz.pl>
2020-10-20 14:25:23 +0200

    fontsize

--- main.py
+++ main.py
@@ -9,10 +9,10 @@ class Application(tk.Frame):
         self.create_widgets()
 
     def create_widgets(self):
-        self.input = tk.Text(self)
+        self.input = tk.Text(self, height=10, width=20, font=('Arial', 30))
         self.input.pack(side='left')
 
-        self.output = tk.Text(self)
+        self.output = tk.Text(self, height=10, width=20, font=('Arial', 30))
         self.output.pack(side='right')
 
         self.encypt = tk.Button(self, text='>>>', command=self.encypt)
@@ -27,12 +27,12 @@ class Application(tk.Frame):
         # TODO implement decrypting function
 
         self.output.delete('1.0', tk.END)
-        self.output.insert(index='1.0', chars=text)
+        self.output.insert(index='1.0height=10, width=20, font=('Arial', 20)', chars=text)
 
     def decrypt(self):
         text = self.output.get('1.0', tk.END)
 
-        # TODO implement decrypting function
+  ion
 
         self.input.delete('1.0', tk.END)
         self.input.insert(index='1.0', chars=text)
